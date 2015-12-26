ce4163949685e05ad76dd01ab8b6ae0e8408bb21
bharathvaj1995 <bharathvaj1995@gmail.com>
2015-12-26 21:27:16 +0530

    Update Sq_matrixTranspose.c

--- Sq_matrixTranspose.c
+++ Sq_matrixTranspose.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<stdlib.h>
 int main()
  {
          int **a;
@@ -13,40 +12,41 @@ int main()
              a[i]=(int *) malloc(sizeof(int *)*n);
          }
 
-        // matrix input
-        printf("\n enter matrix \n",n);
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                scanf("%d",&a[i][j]);
-            }
-        }
-        //output matrix
-        printf("\n Transpose of the given matrix is\n");
-        for(i=0;i<n;i++)
-        {
-            for(j=i;j<n;j++)
-            {
-                temp=a[i][j];
-                a[i][j]=a[j][i];
-                a[j][i]=temp;
-            }
-        }
-        /* to print the array */
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-               printf("%d\t",a[i][j]);
-            }
-            printf("\n");
-        }
-        /* to  release the  memory*/
-        for(i=0;i<n;i++)
-        {
-        free(a[i]);
-        }
-        free(a);
+         /* matrix input */
+         printf("\n enter matrix \n",n);
+         for(i=0;i<n;i++)
+         {
+             for(j=0;j<n;j++)
+             {
+                 scanf("%d",&a[i][j]);
+             }
+         }
+        
+         for(i=0;i<n;i++)
+         {
+             for(j=i;j<n;j++)
+             {
+                 temp=a[i][j];
+                 a[i][j]=a[j][i];
+                 a[j][i]=temp;
+             }
+         }
+         /* to print the matrix */
+         printf("\n Transpose of the given matrix is\n");
+        
+         for(i=0;i<n;i++)
+         {
+             for(j=0;j<n;j++)
+             {
+                 printf("%d\t",a[i][j]);
+             }
+             printf("\n");
+         }
+         /* to  release the  memory*/
+         for(i=0;i<n;i++)
+         {
+             free(a[i]);
+         }
+         free(a);
         return 0;
 }
