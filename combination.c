866e1f64f4aa2f6957a5f71c5c576295fc62f76d
bharathvaj1995 <bharathvaj1995@gmail.com>
2016-01-10 16:35:30 +0530

    Create combination.c

---
+++ combination.c
@@ -0,0 +1,44 @@
+
+/* the array size can be set  from 1 to 100 for the input limit */
+
+#include <stdio.h>
+int main()
+{
+        int p= -1,i,n, a[100], stack[100];
+        printf(" Enter the limit : ");
+        scanf("%d",&n);
+        for (i=0 ; i<n ; i++)
+        {
+                a[i]=i+1;
+        }
+        while(1)
+        {
+                // this loops executes until stack overflows
+                while(p<n-1)
+                {
+                    p=p+1;
+                    stack[p]=1;
+                    for (i=0 ; i<=p ; i++)
+                    {
+                        if (stack[i]==1)
+                        {
+                            printf("%d ",a[i]);
+                        }
+                    }
+                    printf("\n");
+                }
+                // unstack all the zeros until top of stack is equals "1"
+                while (stack[p]==0 && p>=0)
+                {
+                    p=p-1;
+                }
+                // set top of stack  to "0" for each loop iteration
+                stack[p]=0;
+                // stop when bottom is reached
+                if (p<0)
+                {
+                    break;
+                }
+        }
+        return 0;
+}
