9e85e791be5daaaf2507de994dab3e1af64bf406
bharathvaj1995 <bharathvaj1995@gmail.com>
2016-02-11 05:21:32 +0530

    Create sum_of_3nos.c

---
+++ sum_of_3nos.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int fun(int ,int ,int );
+int main()
+{
+    int a,b,c,i,sum = 0;
+    printf("Enter the three numbers");
+	  scanf("%d%d%d",&a,&b,&c);
+    printf("\n Using variables :");
+    printf("\t %d",a+b+c);
+    printf("\n Using array :");
+    int arr[3]={a,b,c};
+    sum=0;
+    for(i=0;i<3;i++)
+    {
+        sum = sum + arr[i];
+    }
+    printf("\t\t %d",sum);
+    sum = 0;
+    printf("\n Using function call :");
+    sum = fun(a,b,c);
+    printf("\t %d",sum);
+    return 0;
+}
+int fun(int a,int b,int c)
+{
+    return a+b+c;
+}
