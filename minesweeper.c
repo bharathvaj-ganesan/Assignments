d86bbbeb5e1b8ac83f270c7dcb8d4898f15c8881
bharathvaj1995 <bharathvaj1995@gmail.com>
2016-02-15 22:55:28 +0530

    Update minesweeper.c

--- minesweeper.c
+++ minesweeper.c
@@ -5,77 +5,76 @@ void ms(int* [],int, int);
 int binarcolumn_rand();
 int main()
 {
-    int row,column,i,j;
-    printf("\n Enter the row and column :");
-    scanf("%d%d",&row,&column);
-    int *arr2d[row];
-    for(i=0;i<row;i++)
-    {
-       arr2d[i]=(int *)malloc(column*sizeof(int));
-
-    }
-    for(i=0;i<row;i++)
-    {
-        for(j=0;j<column;j++)
-        {
-           arr2d[i][j]=0;
-        }
-    }
-    srand(time(NULL));
+    	int row,column,i,j;
+    	printf("\n Enter the row and column :");
+    	scanf("%d%d",&row,&column);
+    	int *arr2d[row];
+    	for(i=0;i<row;i++)
+    	{
+       		arr2d[i]=(int *)malloc(column*sizeof(int));
+    	}
+    	for(i=0;i<row;i++)
+    	{
+        	for(j=0;j<column;j++)
+        	{
+           		arr2d[i][j]=0;
+        	}
+    	}
+    	srand(time(NULL));
 	for(i=0; i<row; i++)
-    {
-        for(j=0; j<column; j++)
+    	{
+        	for(j=0; j<column; j++)
 		{
 		    arr2d[i][j] = binarcolumn_rand();
 		}
-    }
-    printf("Filling '0' with a number corresponding to surrounding mines...\n");
-	ms(arr2d, row, column);
-    for(i=0;i<row;i++)
-    {
-        for(j=0;j<column;j++)
-        {
-           printf("%d\t",arr2d[i][j]);
-        }
-        printf("\n\n\n");
-    }
-
+	 }
+	 printf("Filling '0' with a number corresponding to surrounding mines...\n");
+    	 ms(arr2d, row, column);
+	 for(i=0;i<row;i++)
+	 {
+		 for(j=0;j<column;j++)
+                 {
+           		printf("%d\t",arr2d[i][j]);
+		 }
+	 	printf("\n\n\n");
+    	}
      return 0;
 }
 int binarcolumn_rand()
 {
-	return ((float)rand()/RAND_MAX>0.5)?1:0;
+	return ((float)rand()/RAND_MAX>0.5)?1:0;  //Refered internet for rand() function and "RAND_MAX" macro concept. 
 }
 void ms(int *array[], int row, int column)
 {
 	int i,j,sum;
 	int *cell[row];
 	for(i=0; i<row;i++)
-    {
-        cell[i] = (int*)malloc(column*sizeof(int));
-    }
+    	{
+		 cell[i] = (int*)malloc(column*sizeof(int));
+    	}
 	for(i=0; i<row;i++)
-    {
-        for(j=0;j<column;j++)
-        {
-            cell[i][j] = 0;
-        }
-    }
+	 {
+        	for(j=0;j<column;j++)
+        	{
+            	      cell[i][j] = 0;
+	        }
+         }
 	for(i=0;i<row;i++)
-    {
-        for(j=0; j<column; j++)
         {
+        	for(j=0; j<column; j++)
+        	{
 			sum = 0;
 			if(array[i][j]==0)
 			{
+				//top
 				if(i>0)
-                    sum =sum + array[i-1][j];
+                    		sum =sum + array[i-1][j];
 				//  below
 				if(i<row-1)
-                    sum =sum + array[i+1][j];
+                    		sum =sum + array[i+1][j];
 				//  left
 				if(j>0)
-                    sum =sum + array[i][j-1];
+                    		sum =sum + array[i][j-1];
 				//  right
 				if(j<column-1)
 				 sum =sum + array[i][j+1];
@@ -96,17 +95,17 @@ void ms(int *array[], int row, int column)
 		}
 	}
 	for(i=0; i<row; i++)
-    {
-        for(j=0; j<column; j++)
-		{
+        {
+              for(j=0; j<column; j++)
+	      {
 		    if(!array[i][j])
-            {
-                array[i][j] = cell[i][j];
-            }
-			else
-            {
-                array[i][j] = 'x';
-            }
-		}
-    }
+            	    {
+        		  array[i][j] = cell[i][j];
+        	    }
+		    else
+            	    {
+                	 array[i][j] = 'x';
+            	    }
+	      }
+        }
 }
