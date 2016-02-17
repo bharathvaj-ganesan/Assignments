19eb04589a7cad366992b028525788a2fe2082d7
bharathvaj1995 <bharathvaj1995@gmail.com>
2016-02-17 18:58:23 +0530

    Create ap.c

---
+++ ap.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,s,d,i;
+    printf("Enter the value for N , S and D : ");
+    scanf("%d%d%d",&n,&s,&d);
+
+    for(i=0;i<n-1;i++)
+    {
+        printf("%d\t",s);
+        s=s+d;
+    }
+    return 0;
+}
