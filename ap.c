6eff1452a2bfc62808da438669dc4b9f06e06970
bharathvaj1995 <bharathvaj1995@gmail.com>
2016-02-17 20:28:51 +0530

    Update ap.c

--- ap.c
+++ ap.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,s,d,i;
+    int n,s,s1,d,i;
     printf("Enter the value for N , S and D : ");
     scanf("%d%d%d",&n,&s,&d);
-
+    s1=s;
     for(i=0;i<n;i++)
     {
-        printf("%d\t",s);
-        s=s+d;
+        printf("%d\t",s1);
+        s1=s1+d;
     }
     return 0;
 }
