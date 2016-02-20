422e1cad81ac3d12c48736c0b95bc85c9ab86860
bharathvaj1995 <bharathvaj1995@gmail.com>
2016-02-20 21:17:10 +0530

    Update nxtpal.c

--- nxtpal.c
+++ nxtpal.c
@@ -10,7 +10,7 @@ int main()
 		t=n1%10;
 		n1=n1/10;
 		c++;
-		
+
 	}
 	n1=n;
 	int arr[c],arr1[c];
@@ -26,30 +26,53 @@ int main()
 	{
 		printf("%d\t",arr[i]);
 	}
+	printf("\n");
 	for(i=0;i<c;i++)
 	{
 		arr1[i]=arr[i];
 	}
 	pivot=c/2;
 	j=pivot-1;
-	for(i=pivot+1;i<c;i++)
-	{	
-		arr1[i]=arr1[j];
-		j--;
-	}
-	
-	printf("\n");
-	for(i=0;i<c;i++)
-	{
-		if(arr1[i]<arr[i])
-		{
-			f=1;
-		}
-	}
-	if(f==1)
-	{
-		arr1[pivot]++;	
-	}
+	if(c%2==0)
+    {
+         for(i=pivot;i<c;i++)
+         {
+            arr1[i]=arr1[j];
+            j--;
+          }
+          for(i=0;i<c;i++)
+         {
+            if(arr1[i]<arr[i])
+            {
+                f=1;
+            }
+        }
+        if(f==1)
+        {
+            arr1[pivot]++;
+            arr1[pivot-1]++;
+        }
+    }
+    else
+    {
+         for(i=pivot+1;i<c;i++)
+         {
+            arr1[i]=arr1[j];
+            j--;
+         }
+         for(i=0;i<c;i++)
+         {
+            if(arr1[i]<arr[i])
+            {
+                f=1;
+            }
+        }
+        if(f==1)
+        {
+            arr1[pivot]++;
+        }
+
+    }
 	for(i=0;i<c;i++)
 	{
 		printf("%d\t",arr1[i]);
@@ -57,4 +80,5 @@ int main()
 
 	return 0;
 }
+
 				
